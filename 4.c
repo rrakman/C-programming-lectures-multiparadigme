843cef4f16d26457eb4273a52924c482a6367159
r3daaa <r3da@protonmail.com>
2020-05-02 01:21:45 +0000

    from 0 to hero fuck lel (converting from MAJ to min)

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int	main (int argc, char *argv[])
+{
+	char toto;
+	toto = 65;
+	printf("%c\n", toto);
+	toto = toto + 32;
+	printf("%c\n", toto);
+	return EXIT_SUCCESS;
+}
