7f54f9eef3277e5ddee70c0df7b5aad439580a15
r3daaa <r3da@protonmail.com>
2020-05-06 16:22:50 +0000

    frm 0 to hero fuck lel (using while and for loops)

---
+++ 7-les-boucles.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int	main (int argc, char *argv[])
+{
+
+	int i = 0;
+
+	printf("Here is the while loop xD.\n");
+
+	while(i <= 10)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+
+	printf("Here is the for loop xD.\n");
+
+	 int x;
+	x =0;
+	for (x = 0; x <= 5; x++) {
+		printf("%d\n",x);
+	}
+
+	return EXIT_SUCCESS;
+}
