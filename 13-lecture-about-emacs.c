3a653d3629e99cad8edb5957765a9d489ce6a8cf
r3daaa <r3da@protonmail.com>
2020-05-12 21:45:57 +0000

    from zero to hero(only comments the lectures wasnt practical)

---
+++ 13-lecture-about-emacs.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int	main (int argc, char *argv[])
+{
+/* i dont use emacs so fuck that lecture*/
+	return EXIT_SUCCESS;
+}
---
+++ 14-Numération-binaire.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int	main (int argc, char *argv[])
+{
+/* in this lecture we learnt about data types and how it actually get stored in the memory with binary numbers..etc its interessting even its not practicale right now*/
+	return EXIT_SUCCESS;
+}
