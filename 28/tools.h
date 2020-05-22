3a073f846c2576c1b707b217b20fdd8330d1220c
r3daaa <r3da@protonmail.com>
2020-05-22 02:56:48 +0000

    converting a string to int (only positive numbers -to be improved later)

---
+++ 28/tools.h
@@ -0,0 +1,11 @@
+/*
+	fonction : string to int that accept only positive values
+   without printing any errors if smtg is wrong
+*/
+int string_to_int(char* str);
+/*
+	fonction : safe string to int
+  that prints errors if smtg is wrong
+*/
+
+int safe_string_to_int(char* str,int* ok);
